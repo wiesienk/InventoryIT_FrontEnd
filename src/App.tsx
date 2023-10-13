268b6f1fee7185dc7b6ddb328a328583f1f9b032
Niewinski <niewinski.woj@gmail.com>
2023-10-13 15:14:01 +0200

    Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,26 @@
-import React, {useState, useContext, createContext} from 'react';
-import {Header} from "./components/layouts/Header/Header";
-import {Equipments} from "./components/layouts/pages/Equipment/Equipments";
-import {Users} from "./components/layouts/pages/Users/Users";
-import {Route, Routes} from "react-router-dom";
-import {AddUser} from "./components/layouts/pages/Users/AddUser";
-import {UserDetails} from "./components/layouts/pages/Users/UserDetails";
-import {AddEquipment} from "./components/layouts/pages/Equipment/AddEquipment";
-import {EquipmentDetails} from "./components/layouts/pages/Equipment/EquipmentDetails";
-import {LoginForm} from "./components/layouts/LoginForm/LoginForm";
-import {HeaderButtons} from "./components/layouts/Header/HeaderButtons";
-import {Logout} from "./components/layouts/Logout/Logout";
-import "./App.css"
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-
-
-export const App = () => {
-
-
-
-    const [isLoggedIn, setLoggedIn] = useState(false);
-
-
-
-    const handleStateLogin = () => {
-        setLoggedIn(true);
-    };
-    const handleStateLogout = () => {
-        setLoggedIn(false);
-    };
-
-
-    if (!isLoggedIn) {
-        return <>
-            <Header></Header>
-            <LoginForm onLogin={handleStateLogin}/>
-        </>
-
-    }
-
-    return (<>
-            <Header/>
-            <HeaderButtons/>
-            <Routes>
-                <Route path="/equipments" element={<Equipments/>}/>
-                <Route path="/equipments/add-equipment" element={<AddEquipment/>}/>
-                <Route path="/equipments/:id" element={<EquipmentDetails/>}/>
-                <Route path="/users" element={<Users/>}/>
-                <Route path="/users/add-user" element={<AddUser/>}/>
-                <Route path="/users/:id" element={<UserDetails/>}/>
-                <Route path="/logout" element={<Logout onLogout={handleStateLogout}/>}/>
-            </Routes>
-    </>
-
-    )
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-
-
-
-
+export default App;
