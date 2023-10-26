47b6624013887752989d2031cc1e6038ce13e0df
Niewinski <niewinski.woj@gmail.com>
2023-10-26 15:32:00 +0200

    Install react-router-dom and make paths

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import {Header} from "./components/layouts/Header/Header";
 import {Equipment} from "./components/layouts/pages/Equipment/Equipment";
 import {Users} from "./components/layouts/pages/Users/Users";
+import {Route, Routes} from "react-router-dom";
 
 
 
 export const App = () => {
    return (
-       <>
-           <Header/>
-           <Equipment/>
-           <Users/>
-      </>
+    <>
+        <Header/>
+        <Routes>
+            <Route path="/equipment" element={<Equipment/>}/>
+            <Route path="/users" element={<Users/>}/>
+        </Routes>
+    </>
    )
 }
 
