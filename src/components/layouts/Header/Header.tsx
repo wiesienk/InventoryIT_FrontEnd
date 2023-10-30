0d3cb2be45a4da8e47ce6f5a5eb864beeb3641a7
Niewinski <niewinski.woj@gmail.com>
2023-10-30 10:53:38 +0100

    added new user form

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -4,11 +4,11 @@ import {Btn} from "../../common/Btn";
 export const Header = () => {
     return (
         <header className="header">
-            <h1 className="header__title">Ewidencja sprzętu IT</h1>
-            <div className="header__buttons">
+            <h1 className="header_title">InventoryIT</h1>
+            <div className="header_buttons">
                 <Btn text="Wyposażenie" to="/equipment"/>
                 <Btn text="Użytkownicy" to="/users"/>
-                <Btn text="Wyloguj" to="logout"/>
+                <Btn text="Wyloguj" to="/logout"/>
             </div>
         </header>
     )
