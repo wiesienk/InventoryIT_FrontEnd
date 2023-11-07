870587cd40d336f965acd450e22714214bd43548
Niewinski <niewinski.woj@gmail.com>
2023-11-07 14:55:48 +0100

    Addet to logout deleting cookies

--- src/components/layouts/Logout/Logout.tsx
+++ src/components/layouts/Logout/Logout.tsx
@@ -8,6 +8,7 @@ export const Logout:React.FC<LogoutFormProps> = ({ onLogout } ) => {
     useEffect(() => {
        (async()=>await fetch(`https://http://localhost:3001/auth/logout`))()
     }, []);
+    document.cookie = "jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     window.location.href = "/";
     onLogout()
     return <>Wylogowano</>
